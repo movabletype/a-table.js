fd75c2ff46355b37ad2a841085d1bcbfc19f3011
Masahiro Iuchi <miuchi@sixapart.com>
2024-09-09 16:34:48 +0900

    npm run build:js

--- build/a-table.js
+++ build/a-table.js
@@ -4706,7 +4706,9 @@ var aTable = function (_aTemplate) {
       data.mode = 'col';
       data.selectedColNo = -1;
       data.selectedRowNo = i;
-      this.contextmenu();
+      if (data.increaseDecreaseRows) {
+        this.contextmenu();
+      }
       this.update();
     }
   }, {
@@ -4732,7 +4734,9 @@ var aTable = function (_aTemplate) {
       data.mode = 'row';
       data.selectedRowNo = -1;
       data.selectedColNo = i;
-      this.contextmenu();
+      if (data.increaseDecreaseColumns) {
+        this.contextmenu();
+      }
       this.update();
     }
   }, {
